2fc0f127078e90928ab3b30f675c424faf863c22
Judith <72439973+judicodes@users.noreply.github.com>
2022-06-13 16:43:55 +0300

    Page Layout + Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Helloooo</h1>
-      <button className="btn">Testttt</button>
-    </>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className=" container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/notfound" element={<NotFound />}></Route>
+            <Route path="/*" element={<NotFound />}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,29 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the
+        <a href="https://www.udemy.com/course/modern-react-front-to-back/">
+          {" "}
+          React Front To Back
+        </a>{" "}
+        Udemy course by
+        <strong>
+          <a href="https://traversymedia.com"> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg">
+        Version <span>1.0.0</span>
+      </p>
+      <p className="text-lg">
+        Layout By:
+        <a href="https://twitter.com/hassibmoddasser">Hassib Moddasser</a>
+      </p>
+    </>
+  );
+}
+
+export default About;
